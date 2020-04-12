a01c6f38d83845ec71adc6f238811b4a03810337
Andrei Mušnikov <andrei.musnikov@playtech.com>
2020-04-12 20:17:46 +0200

    first commit of completed app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React, { Fragment } from 'react'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import User from './components/users/User'
+import Alert from './components/layout/Alert'
+import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
+
+import './App.css'
+
+const App = () => {
+	return (
+		<GithubState>
+			<AlertState>
+				<Router>
+					<Fragment>
+						<Navbar />
+						<div className='container'>
+							<Alert />
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<Route exact path='/about' component={About} />
+								<Route exact path='/user/:login' component={User} />
+								<Route component={NotFound} />
+							</Switch>
+						</div>
+					</Fragment>
+				</Router>
+			</AlertState>
+		</GithubState>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+const About = () => (
+	<Fragment>
+		<h1>About this app</h1>
+		<p>App to search Github users</p>
+		<p>Version: 1.0.0</p>
+	</Fragment>
+)
+
+export default About
